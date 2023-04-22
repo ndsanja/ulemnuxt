e68480020f78f560dfd5d2d9f00fa48a70dbc2d5
ndsanja <ndsanja@gmail.com>
2023-04-22 08:28:04 +0700

    add indicator active and hover

--- components/Components/Editor.vue
+++ components/Components/Editor.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 const { dataClasses } = useStore()
-const { editorIsDragStart, editorIsDragging, editorIsDragEnd, editorIsDragOver, useEditorDragStart, useEditorDragMove, useEditorDragEnd } = useEditor()
+const { editorIsDragStart, editorIsDragging, editorIsDragEnd, editorIsDragOver, useEditorDragStart, useEditorDragMove, useEditorDragEnd, editorItemHoverId, editorItemSelectedId } = useEditor()
 
 interface Props {
   data: any
@@ -9,18 +9,38 @@ interface Props {
 
 const props = defineProps<Props>()
 
+const handleMouseOver = (id: any) => {
+  editorItemHoverId.value = id
+}
 
 </script>
 
 
 <template>
-  <ElementEditable v-for="(item, index) in props.data" :key="item.id" :dataItemProps="item" :id="item.id"
-    @mousedown.self="useEditorDragStart($event, item.id)" @mousemove.self.prevent="useEditorDragMove"
-    @mouseup="useEditorDragEnd" @touchstart.self.passive="useEditorDragStart($event, item.id)"
-    @touchmove.self.prevent="useEditorDragMove" @touchend="useEditorDragEnd"
-    :class="`absolute ${item.id != 'root' && editorIsDragging ? 'tw-cursor-move' : ''} ${dataClasses(item?.classes)}`">
+  <ElementEditable v-for="(item, index) in  props.data " :key="item.id" :dataItemProps="item" :id="item.id"
+    :data-el-kind="item.elKind" :data-item-id="item.id" @mouseover.self="handleMouseOver(item.id)"
+    @mousedown.self.prevent="useEditorDragStart($event, item.id)" @mousemove.self.prevent="useEditorDragMove"
+    @mouseup.self.prevent="useEditorDragEnd" @touchstart.self.passive="useEditorDragStart($event, item.id)"
+    @touchmove.self.prevent="useEditorDragMove" @touchend.self.prevent="useEditorDragEnd"
+    :class="`${dataClasses(item?.classes)} ${editorItemSelectedId == item.id && !editorIsDragging ? 'selected' : ''} ${editorItemHoverId == item.id && 'hovered'} ${editorIsDragStart && editorItemSelectedId == item.id && item.elKind != 'section' && item.id != 'root' ? 'dragging' : ''}`">
     <ComponentsEditor :data="item.children" />
   </ElementEditable>
 </template>
 
-<style scoped></style>
+<style scoped>
+.selected {
+  outline: 1px blue solid;
+  outline-offset: 1px;
+}
+
+.dragging {
+  cursor: move;
+  opacity: 0.8;
+  position: absolute;
+}
+
+.hovered {
+  outline: 1px blue solid;
+  outline-offset: 1px;
+}
+</style>
--- composables/useEditor.ts
+++ composables/useEditor.ts
@@ -5,6 +5,13 @@ export const useEditor = () => {
   const editorIsDragOver = useState('is-drag-over', () => false);
   const editorIsDragCancel = useState('is-drag-cancel', () => false);
   const editorItemId = useState('item-id', () => '');
+  const editorParentSection = useState('parent-section', () => '');
+  const editorItemActiveId = useState<any>('active-id', () => '');
+  const editorItemSelectedId = useState<any>('active-id', () => '');
+  const editorItemHoverId = useState<any>('hover-id', () => '');
+  const editorDragItem = useState<any>('drag-item', () => null);
+  const editorDropItem = useState<any>('drop-item', () => null);
+  const editorDragOverItem = useState<any>('drag-over-item', () => null);
 
   const useEditorDragStart = (e: any, id: any) => {
     editorIsDragStart.value = true;
@@ -12,11 +19,23 @@ export const useEditor = () => {
     editorIsDragEnd.value = false;
     editorIsDragOver.value = false;
     editorIsDragCancel.value = false;
+
     editorItemId.value = id;
+
+    editorItemActiveId.value = id;
+    editorItemSelectedId.value = id;
+    editorDragItem.value = e.target;
+    editorParentSection.value =
+      editorDragItem.value.attributes['data-item-id'].value;
   };
 
   const useEditorDragMove = (e: any) => {
-    if (!editorIsDragStart.value) return;
+    if (
+      !editorIsDragStart.value ||
+      editorItemId.value == 'root' ||
+      editorDragItem.value.attributes?.['data-el-kind']?.value == 'section'
+    )
+      return;
     editorIsDragging.value = true;
     // @ts-ignore
     $(function () {
@@ -37,6 +56,8 @@ export const useEditor = () => {
   };
 
   const useEditorDragEnd = (e: any) => {
+    editorDropItem.value = e.target;
+
     editorIsDragStart.value = false;
     editorIsDragging.value = false;
     editorIsDragEnd.value = true;
@@ -51,6 +72,12 @@ export const useEditor = () => {
     editorIsDragEnd,
     editorIsDragCancel,
     editorItemId,
+    editorItemActiveId,
+    editorItemSelectedId,
+    editorItemHoverId,
+    editorDragItem,
+    editorDropItem,
+    editorDragOverItem,
     useEditorDragStart,
     useEditorDragMove,
     useEditorDragEnd,
